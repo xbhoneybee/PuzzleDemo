9715fadbe8dff6e2971acf8627098bc670792125
X <qq383441907@outlook.com>
2017-07-15 14:54:41 +0800

    add menu

--- Puzzle/app/src/main/java/com/example/bjtu/puzzle/MainActivity.java
+++ Puzzle/app/src/main/java/com/example/bjtu/puzzle/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.bjtu.puzzle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int Difficulty;
+    private int Difficulty=3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,4 +22,30 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("Picture",R.drawable.bjtu);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.id_easy:
+                Difficulty=3;
+                //Toast.makeText(this,"You clicked"+Difficulty,Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_medium:
+                Difficulty=4;
+                //Toast.makeText(this,"You clicked"+Difficulty,Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_hard:
+                Difficulty=5;
+                //Toast.makeText(this,"You clicked"+Difficulty,Toast.LENGTH_SHORT).show();
+            break;
+            default:
+            break;
+        }
+        return true;
+    }
+
 }
--- Puzzle/app/src/main/java/com/example/bjtu/puzzle/Puzzle.java
+++ Puzzle/app/src/main/java/com/example/bjtu/puzzle/Puzzle.java
@@ -3,19 +3,23 @@ package com.example.bjtu.puzzle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 public class Puzzle extends AppCompatActivity {
 
+    private static final String TAG = "Puzzle";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle);
         Intent intent=getIntent();
-        int picture =0,n=0;
+        int picture =0,n=2;
         picture=intent.getIntExtra("Picture",picture);
         n=intent.getIntExtra("Difficulty",n);
         ImageView imageView=(ImageView) findViewById(R.id.pictureView);
         imageView.setImageResource(picture);
+        Log.e(TAG, "onCreate: "+n );
+
     }
 }
