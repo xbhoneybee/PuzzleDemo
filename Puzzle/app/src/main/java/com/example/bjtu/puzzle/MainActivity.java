7b226351cbc8e182fda7cbd8c87931a02d507740
X <qq383441907@outlook.com>
2017-07-17 19:18:42 +0800

    ad

--- Puzzle/app/src/main/java/com/example/bjtu/puzzle/MainActivity.java
+++ Puzzle/app/src/main/java/com/example/bjtu/puzzle/MainActivity.java
@@ -1,6 +1,16 @@
 package com.example.bjtu.puzzle;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,7 +22,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -66,6 +79,105 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+    //添加几个方法
+    private void showDialogItem(){
+        /*
+         *选择使用相机还是使用本地图库
+         * 使用AlertDialog比较合适
+         */
+        AlertDialog.Builder dialog= new AlertDialog.Builder(MainActivity.this);
+        dialog.setTitle("请选择");
+        dialog.setItems(new String[]{"使用相机", "本地图库"}, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case 0:
+                        //camera
+                        Intent intent0 =new Intent("MediaStroe.IMAGE_CAPTURE");
+                        long  time = Calendar.getInstance().getTimeInMillis();
+
+                        intent0.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(TEMP_IMAGE_PATH+time+".jpg")));
+                        startActivityForResult(intent0,RESULT_CAMERA);
+                        break;
+                    case 1:
+                        //gallery
+                        Intent intent1 =new Intent(Intent.CATEGORY_OPENABLE);
+                        intent1.setType(IMAGE_TYPE);
+                        startActivityForResult(intent1,RESULT_GALLERY);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        dialog.create().show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode){
+            case RESULT_CAMERA:
+                if(resultCode==RESULT_OK) {
+                    Intent intent=new Intent(MainActivity.this,Puzzle.class);
+                    intent.putExtra("Difficulty",this.Difficulty);
+                    intent.putExtra("Picture",TEMP_IMAGE_PATH);
+                    startActivity(intent);
+                }
+                break;
+            case RESULT_GALLERY:
+                if(resultCode==RESULT_OK) {
+                    Cursor cursor = this.getContentResolver().query(data.getData(), null, null, null, null);
+                    cursor.moveToFirst();
+                    String imagePath = cursor.getString(cursor.getColumnIndex("_data"));
+                    Intent intent=new Intent(MainActivity.this,Puzzle.class);
+                    intent.putExtra("Difficulty",this.Difficulty);
+                    intent.putExtra("Picture",imagePath);
+                    startActivity(intent);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Bitmap 和Drawable 的转化
+     */
+    private Bitmap PathToBitmap(String path)  {
+        Uri imguri=Uri.fromFile(new File(path));
+        Bitmap bitmap= null;
+        try {
+            bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imguri));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+    private   Bitmap DrawableIdToBitmap(int id) {
+        Drawable drawable= ContextCompat.getDrawable(this,id);
+        Bitmap bitmap = Bitmap.createBitmap(
+                drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(),
+                drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+                        : Bitmap.Config.RGB_565);
+        Canvas canvas = new Canvas(bitmap);
+        //canvas.setBitmap(bitmap);
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+    public static Bitmap DrawableToBitmap(Drawable drawable) {
+        Bitmap bitmap = Bitmap.createBitmap(
+                drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(),
+                drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+                        : Bitmap.Config.RGB_565);
+        Canvas canvas = new Canvas(bitmap);
+        //canvas.setBitmap(bitmap);
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        drawable.draw(canvas);
+        return bitmap;
     }
     //初始化图片
     public void initImage(){
